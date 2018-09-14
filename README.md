1cbe6def875b1610ad1ed74d40e4054be1192c0d
Luiz Fantin <fantin_luiz@outlook.com>
2018-09-14 00:52:41 -0300

    Versão 1.1.0 (Novas Funcionalidades)

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 "# emprestimo" 
 
-Versão (1.0.0):
-
+Versão (1.0.0):                                                                                         
 	Solicitar as seguintes informações de um empréstimo de cliente: nome do cliente, CPF do cliente, data de realização do pedido do empréstimo, tipo do empréstimo (imóvel novo, reforma imóvel, carro, viagem, estudo), valor total do empréstimo, número de parcelas a serem pagas, juros mensal, quantidade de parcelas já pagas. Calcular o valor da parcela mensal (valor total do empréstimo / número de parcelas), aplicando os juros mensais).
 
 Versão (1.0.1):
 	Foi adicionado a exibição dos dados ao final do cadastro.
+
+Versão (1.1.0):
+	Foram adicionada novas funcionalidades, tais como:                                                             
+	Incluir condicionais aninhados para indicar a situação do cliente no empréstimo (aprovado, aprovado após análise, reprovado). Se o valor da parcela mensal for menor/igual a 10% da renda mensal (nova informação solicitada) então o empréstimo está aprovado. Se o valor da parcela mensal for maior do que 30% da renda mensal então o empréstimo está reprovado. Se o valor da parcela mensal, for maior do que 10% e menor/igual a 30% então verifica se o cliente tem imóvel próprio quitado (nova informação solicitada) e nenhum empréstimo anterior em aberto (nova informação solicitada). Se sim então o empréstimo está aprovado após análise. Se não, o empréstimo está reprovado após análise.  Um empréstimo está quitado quando a quantidade de parcelas do empréstimo é igual a quantidade de parcelas pagas (assim, ao solicitar se há empréstimo anterior em aberto deve-se solicitar as duas quantidades de parcelas envolvidas e realizar o cálculo necessário). 
--- SistemaEmprestimo_v0.c
+++ SistemaEmprestimo_v0.c
@@ -6,36 +6,46 @@
                       Luiz Fantin Neto
                       Paulo Otávio Godio
                       Vitor Lohan Brito Teixeira
-
-    Versão Atual: (1.0.1)
+    Data: 14/09/2018
+    Versão: (1.1.0)
            Mudanças:
-               - Adicionado sistema de requisição de dados dos clientes
-
+               - Foram adicionados testes condicionais para analisar a possibilidade do empréstimo.
+               Podendo ser: Aprovado, Aprovado após análise, Reprovado após análise, Reprovado.
+    Versões Anteriores : (1.0.1) e (1.0.0)
 */
 
 /*Importação de bibliotecas*/
-//Entrada e saída de dados (utilizadas: scanf(), printf(), fflush())
+//Entrada e saída de dados (utilizadas: scanf(), printf(), fflush()).
 #include<stdio.h>
-//Operacoes matematicas (utilizadas: pow())
+//Operacoes matematicas (utilizadas: pow()).
 #include<math.h>
-//Para usar getch().
+//Para utilizar o getch().
 #include<conio.h>
+//Para manipulação de strings.
+#include<string.h>
 
 /*Funcao principal*/
 int main()
 {
     /*Variáveis que armazenam os dados do cliente*/
     char nomeCliente[51] = {' '};
-    char  CPF[15] = {0};
-    int dataEmprestimo_dia = 0;
-    int dataEmprestimo_mes = 0;
-    int dataEmprestimo_ano = 0;
+    char  cpf[15] = {0};
+    char resultadoEmprestimo[23] = {' '};
+    int dataEmprestimoDia = 0;
+    int dataEmprestimoMes = 0;
+    int dataEmprestimoAno = 0;
     int tipoEmprestimo = 0;
     float valorEmprestimo = 0.0;
     int numParcelas = 0;
     float jurosParcelas = 0.0;
     int parcelasPagas = 0;
     float valorParcela = 0.0;
+    float rendaMensal = 0.0;
+    int imovelQuitado = 0;
+    int emprestimoAnterior = 0;
+    int parcelaAnterior = 0;
+    int parcelaPagaAnterior = 0;
+
 
 
     /*Cabecalho da interface do sistema*/
@@ -54,16 +64,16 @@ int main()
     fflush(stdin);
 
     printf("Insira o CPF do cliente (XXX.XXX.XXX-XX)\n: ");
-    scanf(" %14[^\n]s", CPF);
+    scanf(" %14[^\n]s", cpf);
     fflush(stdin);
 
     printf("Insira a data em que o emprestimo foi realizado\n");
     printf("dia(XX)\n: ");
-    scanf("%d",&dataEmprestimo_dia);
+    scanf("%d",&dataEmprestimoDia);
     printf("mes(XX)\n: ");
-    scanf("%d",&dataEmprestimo_mes);
+    scanf("%d",&dataEmprestimoMes);
     printf("ano(XXXX)\n: ");
-    scanf("%d",&dataEmprestimo_ano);
+    scanf("%d",&dataEmprestimoAno);
 
     printf("Insira o tipo de emprestimo (X)\n");
     printf("1 - Aquisicao de imovel\n");
@@ -85,25 +95,76 @@ int main()
     printf("Insira o numero de parcelas do emprestimo que ja foram pagas (XX)\n: ");
     scanf("%d", &parcelasPagas);
 
+    printf("Insira o valor de sua renda mensal RS(XXXXX.XX)\n: ");
+    scanf("%f", &rendaMensal);
+
     /*Calculo da parcela mensal a ser paga*/
     jurosParcelas /= 100;
     //valorParcela =  (valorEmprestimo/numParcelas)*(1+jurosParcelas);
     valorParcela = (jurosParcelas*valorEmprestimo)/(1- pow(1+jurosParcelas, -numParcelas));
 
-    /*mostra as informações armazenadas ao usuario*/
+    //Se o valor da parcela for menor ou igual a 10% da renda mensal, o empréstimo está aprovado.
+    if(valorParcela<=rendaMensal*0.1)
+        strcpy(resultadoEmprestimo,"Aprovado");
 
+    //Se não, se o valor da parcela for maior que 30% da renda mensal, o empréstimo está reprovado.
+    else if(valorParcela>rendaMensal*0.3)
+        strcpy(resultadoEmprestimo,"Reprovado");
+
+    //Se estiver entre 10% e 30% da renda mensal, é necessario pedir ao usuário novos dados.
+    else
+    {
+        printf("O senhor(a) possue imovel proprio quitado? (1 - Sim ou 0 - Nao\n: ");
+        scanf("%d",&imovelQuitado);
+        if(imovelQuitado==0)
+        {
+            strcpy(resultadoEmprestimo,"Reprovado apos analise");
+        }
+        else
+        {
+            printf("O senhor(a) possue emprestimo anterior? (1 - Sim ou 0 - Nao\n: ");
+            scanf("%d",&emprestimoAnterior);
+
+            //Se o cliente não tiver empréstimo anterior, ele está aprovado após análise.
+            if(emprestimoAnterior==0)
+            {
+                strcpy(resultadoEmprestimo,"Aprovado apos analise");
+            }
+
+            //Se ele possuir empréstimo anterior, é necessário saber se ele já pagou todas as parcelas pendentes.
+            else
+            {
+                printf("Insira o numero de parcelas do emprestimo anterior (XX)\n: ");
+                scanf("%d", &parcelaAnterior);
+
+                printf("Insira o numero de parcelas pagas do emprestimo anterior (XX)\n: ");
+                scanf("%d", &parcelaPagaAnterior);
+
+                //Se ele não tiver quitado o empréstimo anterior, o empréstimo é reprovado após análise.
+                if((parcelaAnterior-parcelaPagaAnterior!=0))
+                    strcpy(resultadoEmprestimo,"Reprovado apos analise");
+
+                //Já se ele tiver quitado o empréstimo anterior, é aprovado após análise.
+                else
+                    strcpy(resultadoEmprestimo,"Aprovado apos analise");
+            }
+        }
+    }
+
+
+    /*mostra as informações armazenadas ao usuario*/
     printf("\nDADOS REGISTRADOS\n\n");
 
     printf("Nome do Cliente\n");
     printf("\t%s\n", nomeCliente);
 
     printf("CPF\n");
-    printf("\t%s\n", CPF);
+    printf("\t%s\n", cpf);
 
     printf("Data de realizacao do emprestimo\n");
-    printf("\t%d/%d/%d\n", dataEmprestimo_dia, dataEmprestimo_mes, dataEmprestimo_ano);
+    printf("\t%d/%d/%d\n", dataEmprestimoDia, dataEmprestimoMes, dataEmprestimoAno);
 
-    printf("Tipo do emprestimo :\t %d\n", tipoEmprestimo);
+    printf("Tipo do emprestimo : %d\n", tipoEmprestimo);
     printf("\t1 - Aquisicao de imovel\n");
     printf("\t2 - Reforma de imovel\n");
     printf("\t3 - Aquisicao de veiculo\n");
@@ -125,6 +186,8 @@ int main()
     printf("Valor da parcela\n");
     printf("\tRS %.2f\n", valorParcela);
 
+    printf("Status do Emprestimo\n");;
+    printf("\t%s\n",resultadoEmprestimo);
 
     getch();
 
